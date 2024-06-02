10c8404347d6e2b1c55c959c8deab59812b1310f
Leekb0804 <lkb8713@gmail.com>
2024-06-02 22:59:46 +0900

    edit q1001 start.sh

--- q1001_linuxmaster/start.sh
+++ q1001_linuxmaster/start.sh
@@ -12,7 +12,7 @@ echo -e "\x1b[38;2;255;255;0m    \|__|     \|_______|\|_______| \x1b[0m"
 echo " "
 echo $stage
 echo " "
-echo "You can learn [ passwd ]"
+echo "You can learn linuxmaster previous questions"
 
 cd /root
 #chmod 700 /home
